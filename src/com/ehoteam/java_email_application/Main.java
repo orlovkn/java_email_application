b90ef30f5ebd939dcaf90253ddf9ceeb8bd182d3
Konstantin Orlov <orlovkn.job@gmail.com>
2020-03-04 07:21:54 +0300

    final

---
+++ src/com/ehoteam/java_email_application/Main.java
@@ -0,0 +1,11 @@
+package com.ehoteam.java_email_application;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Email em1 = new Email("Konstantin", "Orlov");
+        System.out.println (em1.showInfo ());
+
+    }
+}
